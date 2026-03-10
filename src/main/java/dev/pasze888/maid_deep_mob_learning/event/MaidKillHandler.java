a3eb5f223052778dea38b1d84d244d3090dfb1fa
pasze <pasze888@yeah.net>
2026-03-10 20:27:05 +0800

    curios兼容

--- src/main/java/dev/pasze888/maid_deep_mob_learning/event/MaidKillHandler.java
+++ src/main/java/dev/pasze888/maid_deep_mob_learning/event/MaidKillHandler.java
@@ -6,60 +6,63 @@ import dev.shadowsoffire.hostilenetworks.HostileConfig;
 import dev.shadowsoffire.hostilenetworks.HostileEvents;
 import dev.shadowsoffire.hostilenetworks.item.DeepLearnerItem;
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModList;
 import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
-import net.neoforged.neoforge.items.IItemHandler;
 import net.neoforged.neoforge.items.ItemStackHandler;
 import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.SlotResult;
 import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 
-
 // 独立的事件监听类
 public class MaidKillHandler {
+    /*
+    女仆杀死怪物时增加对应模型进度
+    */
     @SubscribeEvent
-    public void onLivingDeath(LivingDeathEvent event) {
+    public void kill(LivingDeathEvent event) {
         // 配置检查
         if (!HostileConfig.killModelUpgrade) return;
-        LivingEntity killed = event.getEntity();
-        // 若击杀者是女仆
-        if (killed.getKillCredit() instanceof EntityMaid maid) {
 
-            //获取怪物类型
-            EntityType<?> killedType = killed.getType();
+        // 击杀者必须是女仆
+        if (!(event.getSource().getEntity() instanceof EntityMaid maid)) return;
 
-            //只返回可用部分
-            ItemStackHandler backpack = maid.getMaidInv();
+        var killedType = event.getEntity().getType();
+
+        // 主手、副手
+        // 由于主手需要持有武器，这里排除主手
+        // checkAndUpdate(maid.getMainHandItem(), killedType);
+        checkAndUpdate(maid.getOffhandItem(), killedType);
+
+        // 背包遍历
+         ItemStackHandler backpack = maid.getMaidInv();
+//        IItemHandler backpack = maid.getAvailableInv(false);
 
             for (int i = 0; i < backpack.getSlots(); i++) {
-
-                if (backpack.getStackInSlot(i).getItem() instanceof DeepLearnerItem){
-                    HostileEvents.updateModels(backpack.getStackInSlot(i),killedType,0);
-                }
-
+                checkAndUpdate(backpack.getStackInSlot(i), killedType);
             }
 
-            /*
- 若安装了curios
-            if (ModList.get().isLoaded("curios")){
-                ICuriosItemHandler inv = CuriosApi.getCuriosInventory(maid).orElse(null);
-                if (inv!=null){
-                    SlotResult res = inv.findFirstCurio(Hostile.Items.DEEP_LEARNER.value()).orElse(null);
-                    if (res!=null){
-                        ItemStack stack = res.stack();
-                        if (stack.is(Hostile.Items.DEEP_LEARNER)){
-                            HostileEvents.updateModels(stack,killedType,0);
-                            inv.setEquippedCurio(res.slotContext().identifier(),res.slotContext().index(),stack);
-                        }
+
+        // 若有饰品栏，类似处理...
+        if (ModList.get().isLoaded("curios")){
+            ICuriosItemHandler inv = CuriosApi.getCuriosInventory(maid).orElse(null);
+            if (inv != null) {
+                SlotResult result = inv.findFirstCurio(Hostile.Items.DEEP_LEARNER.value()).orElse(null);
+                if (result != null) {
+                    ItemStack stack = result.stack();
+                    if (stack.getItem() instanceof DeepLearnerItem) {
+                        HostileEvents.updateModels(stack, killedType, 0);
+                        inv.setEquippedCurio(result.slotContext().identifier(), result.slotContext().index(), stack);
                     }
                 }
             }
-*/
-
         }
     }
 
+    private void checkAndUpdate(ItemStack stack, EntityType<?> killedType) {
+        if (stack.getItem() instanceof DeepLearnerItem) {
+            HostileEvents.updateModels(stack, killedType, 0);
+        }
+    }
 }
